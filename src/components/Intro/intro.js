d36b75ed68c6519172dff062c657364bc4a4adad
Mahesh Suthar <sutharmahesh9780@gmail.com>
2024-03-05 12:29:57 +0530

    khatam

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,7 +2,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect, useRef } from "react";
 import bg from "../../assets/character1.png";
-import Example from "../../assets/pdfs/Cv.PDF";
+import Example from "../../assets/pdfs/mycv.PDF";
 import "./intro.css";
 import git from "../../assets/git.png";
 import insta from "../../assets/insta.png";
@@ -30,21 +30,21 @@ const Intro = () => {
       className="flex flex-col items-center justify-center bg-blue-900 lg:flex-row mt-9 lg:h-screen"
     >
       <div
-        className="p-8 text-center lg:mt-8 lg:w-1/2 lg:text-left "
+        className="p-8 mt-4 text-center lg:mt-8 lg:w-1/2 lg:text-left "
         data-aos="fade-down"
         id="repeat"
       >
-        <span className="flex text-3xl font-extrabold text-center ml-[10rem] lg:font-extrabold lg:text-6xl lg:ml-[0] md:ml-[0] ">
+        <span className=" text-3xl font-extrabold text-center lg:font-extrabold lg:text-6xl lg:ml-[0] md:ml-[0] p-0">
           Hello,
         </span>
-        <span className="flex flex-row ml-[8rem] text-2xl font-extrabold  text-green-300 lg:ml-[0] lg:text-6xl md:ml-[0]">
+        <span className="flex justify-center text-3xl font-extrabold lg:justify-start text-center text-green-300 lg:text-6xl md:ml-[0] lg:ml-[0] p-0 ">
           I'm Mahesh
         </span>{" "}
-        <span className="flex flex-row ml-[5rem] text-2xl font-extrabold  text-green-300 lg:ml-[0] lg:text-5xl md:ml-[0] ">
+        <span className="flex  text-center justify-center lg:justify-start text-3xl font-extrabold  text-green-300  lg:text-5xl md:ml-[0] lg:ml-[0]">
          Frontend Developer
         </span>
         
-        <div className="">
+        <div className="mt-[-20px] lg:mt-[0] md:mt-[0]">
         <p className=" text-lg font-bold p-[3rem]  lg:p-[0] ">
       BCA graduate from Silver Oak  University with
       a CGPA of 8.80. Passionate about web development, I have honed my skills in
@@ -68,7 +68,7 @@ const Intro = () => {
         <div className="w-[400px] flex flex-row">
           <button
             onClick={handleClick}
-            className="h-[50px] w-[120px]  mx-auto text-sm lg:ml-[1px] lg:mt-7 mt-4 cursor-progress border border-yellow-300 rounded-lg hover:bg-green-800 hover:duration-700 lg:ml-0 justify-center mx-auto "
+            className="h-[50px] w-[170px]   md:justify-center  text-sm justify-center items-center lg:justify-start lg:mt-2 mt-2 cursor-progress border border-yellow-300 rounded-lg hover:bg-green-800 hover:duration-700 lg:ml-0  mx-auto "
             id="btn"
           >
             Download CV
